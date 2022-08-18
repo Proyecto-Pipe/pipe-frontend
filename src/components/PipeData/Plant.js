a6f08ed837a11c5cecb51b8f8b537a6c2ff16683
santigo171 <santibetancourt17@gmail.com>
2022-08-18 16:42:48 -0500

    THREE.js debugging canvas

--- src/components/PipeData/Plant.js
+++ src/components/PipeData/Plant.js
@@ -1,29 +1,76 @@
 import React from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 import "./Plant.css";
+
+function addPlantModel(url) {
+  return new Promise((resolve, reject) => {
+    const loader = new GLTFLoader();
+    loader.load(
+      url,
+      (gltf) => {
+        resolve(gltf.scene);
+      },
+      undefined,
+      function (error) {
+        reject(error);
+      }
+    );
+  });
+}
 
 function Plant({ url }) {
   const divRef = React.useRef(null);
 
   React.useEffect(() => {
-    console.log(divRef.current.offsetWidth);
     if (divRef.current == null) return;
+
+    // Basic items
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
+      55,
       divRef.current.offsetHeight / divRef.current.offsetWidth,
       0.1,
       1000
     );
+    camera.position.setZ(30);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(divRef.current.offsetWidth, divRef.current.offsetHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
     divRef.current.appendChild(renderer.domElement);
+
+    // Model
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+    const torus = new THREE.Mesh(geometry, material);
+    scene.add(torus);
+
+    async function insertPlantModel() {
+      const plantModel = await addPlantModel(url);
+      scene.add(plantModel.scene);
+    }
+    insertPlantModel();
+
+    // Light
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(5, 50, 5);
+    scene.add(pointLight);
+
+    // Helpers
+    const lightHelper = new THREE.PointLightHelper(pointLight);
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(lightHelper, gridHelper);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
 
     function animate() {
       requestAnimationFrame(animate);
+      torus.rotation.x += 0.01;
       renderer.render(scene, camera);
+      controls.update();
     }
     animate();
 
